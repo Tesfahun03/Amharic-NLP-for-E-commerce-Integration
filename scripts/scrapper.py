3ea3983d174ce10d0185245951ef7411c7eac046
Tesfahun03 <tesaf1035@gmail.com>
2025-01-17 14:31:04 +0300

    modifiying telegram scrapper

--- scripts/scrapper.py
+++ scripts/scrapper.py
@@ -1,4 +1,5 @@
 import os
+import asyncio
 import json
 import sys
 import logging
@@ -16,28 +17,31 @@ load_dotenv()
 
 api_id = os.getenv('api_id')
 api_hash = os.getenv('api_hash')
-channel_username = '@ZemenExpress'
+channel_usernames = ['@ZemenExpress', '@AwasMart',
+                     '@modernshoppingcenter', '@Shewabrand', '@sinayelj']
 
-client = TelegramClient('session_name', api_id, api_hash)
+
+async def fetch_messages(client, channel_username):
+    messages = []
+    async for message in client.iter_messages(channel_username, limit=100):
+        messages.append(message.text)
+    return messages
 
 
 async def main():
-    await client.start()
+
     logger.info('client connected successfully ')
 
     # Fetch message from the channels above
-    messages = []
-    logger.info(f'fetching data from {channel_username}...')
-    async for msg in client.iter_messages(channel_username, limit=100):
-        if msg:
-            messages.append({
-                'text': msg.text,
-                'sender': msg.sender_id,
-                # 'date': msg.date,
-                'media': msg.photo
-            })
-            # logger.info(
-            #     f"Message Fetched ftom {msg.sender_id} message ")
+    async with TelegramClient('session_name', api_id, api_hash) as client:
+        tasks = [fetch_messages(client, username)
+                 for username in channel_usernames]
+        results = await asyncio.gather(*tasks)
+
+        # Flatten the list of messages and process as needed
+        all_messages = [msg for sublist in results for msg in sublist]
+
+        logger.info(f"messgaes: {all_messages}")
 
     # save message in csv format
     output_file = '../data/telegram_messages.csv'
@@ -51,21 +55,10 @@ async def main():
         if choice != 'y':
             print('opration cancelled')
 
-    # def download_photo(message, file_path):
-    #     if message.photo:
-    #         message.download_media(file=file_path)
-    #         print(f"Photo downloaded to {file_path}")
-
-    # await download_photo(msg, media_file)
-    # saving fetched data into pandas dataframe
-    df = pd.DataFrame(messages)
+    df = pd.DataFrame(all_messages, columns=['messages'])
     df.to_csv(output_file, index=False)
     logger.info(f"messages saved to {output_file}")
 
-    logger.info(
-        f"Message Fetched ftom {msg.sender_id} message {messages[0]['media']}")
-
 
 async def fetch_data():
-    async with client:
-        await main()
+    await main()
