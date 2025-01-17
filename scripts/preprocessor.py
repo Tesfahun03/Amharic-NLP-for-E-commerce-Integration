bbd2102b4cba50cc64d9de26127717fadf602d59
Tesfahun03 <tesaf1035@gmail.com>
2025-01-17 16:41:50 +0300

    fixing bug in preprocessor

--- scripts/preprocessor.py
+++ scripts/preprocessor.py
@@ -4,7 +4,7 @@ import os
 sys.path.append(os.path.abspath('..'))
 
 
-class RemoveUnwanted():
+class RemoveUnwanted:
     """
     A class used to preprocess text data by removing unwanted elements such as special characters,
     URLs, and English words.
@@ -15,15 +15,14 @@ class RemoveUnwanted():
     remove_english_words_from_text(text)
         Removes all English words from a given text.
     """
-    def remove_special_character(messages):
-        cleaned_data = []
-        for message in messages:
-            # Remove emojis, URLs, and special characters
-            text = re.sub(r'http\S+|www\S+|https\S+', '', message)
-            text = re.sub(r'[^\w\s]', '', text)
-            cleaned_data.append(text)
-        return cleaned_data
 
-    def remove_english_words_from_text(text):
+    def remove_special_character(self, message):
+        # Remove emojis, URLs, and special characters
+        text = re.sub(r'http\S+|www\S+|https\S+', '', message)
+        text = re.sub(r'[^\w\s]', '', text)
+        text = text.strip()
+        return text
+
+    def remove_english_words_from_text(self, text):
         # Replace all English words with an empty string
         return re.sub(r'\b[a-zA-Z]+\b', '', text)
